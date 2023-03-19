4ef78ebc1e509c27e617f2a024de5edb440b29dd
SmallPeenwow <bowdencamryn@gmail.com>
2023-03-19 22:39:45 +0200

    React Three.js Tutorial For Beginners Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { MeshDistortMaterial, OrbitControls, Sphere } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 import React from 'react';
 import styled from 'styled-components';
 import Navbar from './Navbar';
@@ -100,7 +102,14 @@ const Hero = () => {
 					<Button>Learn More</Button>
 				</Left>
 				<Right>
-					{/* 3D model */}
+					<Canvas>
+						<OrbitControls enableZoom={false} />
+						<ambientLight intensity={1} />
+						<directionalLight position={[3, 2, 1]} />
+						<Sphere args={[1, 100, 200]} scale={2.4}>
+							<MeshDistortMaterial color='#3d1c56' attach='material' distort={0.5} speed={2} />
+						</Sphere>
+					</Canvas>
 					<Img src='./img/moon.png' />
 				</Right>
 			</Container>
